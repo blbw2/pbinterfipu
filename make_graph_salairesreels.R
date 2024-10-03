004a72f273807485592976ac9cce2c9d11017e77
Benoit Williatte <benoit.williatte@sciencespo.fr>
2024-10-03 12:46:10 +0200

    quasi definitif

--- make_graph_salairesreels.R
+++ make_graph_salairesreels.R
@@ -10,7 +10,7 @@ data_salaires <- read.xlsx("data_salaires.xlsx", sheet = "Sheet1") |>
   mutate(smpt_reel = smpt/hicp,
          obsTime = as.yearqtr(obsTime)) |> 
   group_by(geo) |> 
-  mutate(across(hicp:smpt_reel, ~ (100*./(.[obsTime ==2019.75])-100)))
+  mutate(across(hicp:smpt_reel, ~ (100*./(.[obsTime ==2019.75]))-100))
 
 
 
@@ -106,6 +106,154 @@ graph_Salaires <- ggplot(data_salaires_reshaped2 |> filter((!periode == "endpoin
   theme_ofce()
 
 graph_Salaires
+
+
+##### Version avec 3 périodes #####
+
+ggplot(data_salaires,
+       aes(x=obsTime, y= smpt_reel, colour = geo))+
+  geom_line()+
+  geom_vline(xintercept = 2023)+
+  ofce::theme_ofce()
+
+data_salaires_reshaped3p <- data_salaires|> 
+  select(-hicp, smpt,smpt_reel) |> 
+  pivot_longer(-c(obsTime,geo), names_to = "variable", values_to = "value") |> 
+  group_by(geo,variable) |> 
+  summarise(historique1 = value[obsTime==2022.75],
+            historique2 = value[obsTime == 2024.25]-value[obsTime==2022.75],
+            prev = value[obsTime == 2025.75]-value[obsTime==2024.25],
+            endpoint = value[obsTime == 2025.75]) |> 
+  pivot_longer(historique1:endpoint, names_to="periode", values_to = "value" ) |> 
+  ungroup() |> 
+  mutate(periode = fct_rev(factor(periode, levels = c("historique1","historique2","prev","endpoint"))),
+         geo=factor(geo, levels = c("ITA", "FRA", "DEU", "GBR", "ESP", "USA")),
+         variable =case_when(variable == "smpt" ~ "Salaires nominaux",
+                             variable == "smpt_reel" ~ "Salaires réels"))
+
+
+graph_Salaires <- ggplot(data_salaires_reshaped3p |> filter((!periode == "endpoint")),
+                         aes(y=geo) )+
+  facet_wrap(~variable)+
+  geom_bar(position = "stack", stat = "identity",
+           aes (x=value, y=geo, fill = periode),
+           alpha =1,
+           width= 0.4)+
+  geom_vline(aes(xintercept =0))+
+  geom_point(data=data_salaires_reshaped3p |> filter ((periode == "endpoint")),
+             aes(x = value, y = geo),  size = 2, colour = "firebrick3")+
+  scale_fill_manual(values = c("darkgoldenrod3","royalblue4", "coral3"), 
+                    breaks = c("prev", "historique2", "historique1"),
+                    labels = c("2025 T4- 2024 T2", "2024 T2- 2022 T4", "2022 T4 - 2019 T4"),
+                    # name = "Contributions à\nl'évolution totale\n2024 T4 - 2019 T4",
+                    name = "")+
+  expand_limits(x = c(0,10))+
+  scale_x_continuous(limits = c(-10,35), breaks = seq(-10,35, by = 5))+
+  guides(fill = guide_legend(byrow= TRUE))+
+  labs( x = "Evolution totale (% du niveau 2019 T4)",
+        colour = "", fill = "Croissance des salaires",
+        title = "")+
+  theme_ofce()
+
+
+graph_Salaires
+
+graph_Salaires <- ggplot(data_salaires_reshaped3p |> filter((!periode == "endpoint")),
+                         aes(y=geo) )+
+  facet_wrap(~variable)+
+  geom_bar_interactive(position = "stack", stat = "identity",
+           aes (x=value, y=geo, fill = periode, tooltip = str_c("Contribution à la variation depuis 2019 T4 : <br>",
+                                                                round(value,digits=2),
+                                                                "%")),
+           alpha =1,
+           width= 0.4)+
+  geom_vline(aes(xintercept =0))+
+  geom_point_interactive(data=data_salaires_reshaped3p |> filter ((periode == "endpoint")),
+             aes(x = value, y = geo, tooltip = str_c("Variation depuis 2019 T4 :<br>", round(value,digits=2), "%")),
+                 size = 1, colour = "firebrick3", shape =19
+             )+
+  scale_fill_manual(values = c("darkgoldenrod3","royalblue4", "coral3"), 
+                    breaks = c("prev", "historique2", "historique1"),
+                    labels = c("2025 T4- 2024 T2", "2024 T2- 2022 T4", "2022 T4 - 2019 T4"),
+                    # name = "Contributions à\nl'évolution totale\n2024 T4 - 2019 T4",
+                    name = "")+
+  expand_limits(x = c(0,10))+
+  scale_x_continuous(limits = c(-10,35), breaks = seq(-10,35, by = 5))+
+  guides(fill = guide_legend(byrow= TRUE),
+         colour = FALSE,
+         size = FALSE)+
+  labs( x = "Evolution totale (% du niveau 2019 T4)",
+        y = "",
+        colour = "", fill = "Croissance des salaires",
+        title = "")+
+  theme_ofce()
+
+girafe(ggobj = graph_Salaires,
+       options = list(opts_sizing(rescale = TRUE, width = .5)))
+
+
+graph_Salairesnom <- ggplot(data_salaires_reshaped3p |> filter((!periode == "endpoint"), variable == "Salaires nominaux"),
+                         aes(y=geo) )+
+  geom_bar_interactive(position = "stack", stat = "identity",
+                       aes (x=value, y=geo, fill = periode, tooltip = str_c("Contribution à la variation depuis 2019 T4 : <br>",
+                                                                            round(value,digits=2),
+                                                                            "%")),
+                       alpha =1,
+                       width= 0.4)+
+  geom_vline(aes(xintercept =0))+
+  geom_point_interactive(data=data_salaires_reshaped3p |> filter(periode == "endpoint", variable == "Salaires nominaux"),
+                         aes(x = value, y = geo, tooltip = str_c("Variation depuis 2019 T4 :<br>", round(value,digits=2), "%")),
+                         size = 1, colour = "firebrick3", shape =19
+  )+
+  scale_fill_manual(values = c("darkgoldenrod3","royalblue4", "coral3"), 
+                    breaks = c("prev", "historique2", "historique1"),
+                    labels = c("2025 T4- 2024 T2", "2024 T2- 2022 T4", "2022 T4 - 2019 T4"),
+                    # name = "Contributions à\nl'évolution totale\n2024 T4 - 2019 T4",
+                    name = "")+
+  expand_limits(x = c(0,10))+
+  scale_x_continuous(limits = c(-10,35), breaks = seq(-10,35, by = 5))+
+  guides(fill = guide_legend(byrow= TRUE),
+         colour = FALSE,
+         size = FALSE)+
+  labs( x = "Evolution totale (% du niveau 2019 T4)",
+        y= "",
+        colour = "", fill = "Croissance des salaires",
+        title = "Salaires nominaux")+
+  theme_ofce()
+
+graph_Salairesreels <- ggplot(data_salaires_reshaped3p |> filter((!periode == "endpoint"), variable == "Salaires réels"),
+                            aes(y=geo) )+
+  geom_bar_interactive(position = "stack", stat = "identity",
+                       aes (x=value, y=geo, fill = periode, tooltip = str_c("Contribution à la variation depuis 2019 T4 : <br>",
+                                                                            round(value,digits=2),
+                                                                            "%")),
+                       alpha =1,
+                       width= 0.4)+
+  geom_vline(aes(xintercept =0))+
+  geom_point_interactive(data=data_salaires_reshaped3p |> filter ((periode == "endpoint"), variable == "Salaires réels"),
+                         aes(x = value, y = geo, tooltip = str_c("Variation depuis 2019 T4 :<br>", round(value,digits=2), "%")),
+                         size = 1, colour = "firebrick3", shape =19
+  )+
+  scale_fill_manual(values = c("darkgoldenrod3","royalblue4", "coral3"), 
+                    breaks = c("prev", "historique2", "historique1"),
+                    labels = c("2025 T4- 2024 T2", "2024 T2- 2022 T4", "2022 T4 - 2019 T4"),
+                    # name = "Contributions à\nl'évolution totale\n2024 T4 - 2019 T4",
+                    name = "")+
+  expand_limits(x = c(0,10))+
+  scale_x_continuous(limits = c(-10,35), breaks = seq(-10,35, by = 5))+
+  guides(fill = guide_legend(byrow= TRUE),
+         colour = FALSE,
+         size = FALSE)+
+  labs( x = "Evolution totale (% du niveau 2019 T4)",
+        y= "",
+        colour = "", fill = "Croissance des salaires",
+        title = "Salaires réels")+
+  theme_ofce()
+
+graph_Salairespatchwork <- (graph_Salairesreels+graph_Salairesnom)+
+  plot_layout(guides = "collect")
+
+graph_Salairespatchwork
 
 save(list =ls(pattern = "^graph_.+$"),
      file ="graph_pbinter_salaires.rda")
--- make_graphfipu.R
+++ make_graphfipu.R
@@ -5,5 +5,143 @@ library(ggh4x)
 library(plotly)
 library(ggiraph)
 
-deficits_data <- read.xlsx("decompo_deficits.xlsx", sheet = "Deficits %PIB") |> 
-  pivot_longer(-c(geo,mesure), names_to = "annee", values_to = "value")
+dette_data <- read.xlsx("data_fipu.xlsx", sheet = "datafipu") |> 
+  filter(mesure == "dette") |> 
+  pivot_longer(-c(geo,mesure), names_to = "annee", values_to = "value") |> 
+  filter(annee %in% c(2019,2023,2025)) |> 
+  group_by(geo) |> 
+  pivot_wider(names_prefix = "dette_", names_from = "annee", values_from = "value") |> 
+  mutate(consolid = dette_2025-dette_2023,
+         lollipop_start = dette_2023,
+         dette_2023 = dette_2023-dette_2019,
+         across(where(is.numeric), ~ round(., digits = 1))
+  ) |> 
+  pivot_longer(c(dette_2019,dette_2023), names_to = "annee", values_to= "value" ) |> 
+  mutate(annee = factor(str_remove(annee,"dette_"), levels = c("2019", "2023")),
+         tooltip_dette_2019_2023 = case_when(annee == "2023" ~ str_c("Variation de la dette 2019-2023 : <br>", lollipop_start-value[annee=="2019"]),
+                                             annee == "2019" ~ str_c("Dette 2019 :<br>", value[annee=="2019"])))
+
+
+graph_dette_pbinter <-
+  (ggplot(dette_data |> drop_na(),
+          aes(x=geo, y = value,
+              fill = fct_rev(annee),
+              data_id=annee,
+              tooltip=tooltip_dette_2019_2023))+
+     geom_bar_interactive(stat="identity", position = "stack")+
+     theme_ofce()+
+     scale_fill_manual(values = c("firebrick3", "dodgerblue4"), name = "")+
+     xlab("")+
+     ylab("% du PIB")+
+     coord_cartesian(ylim= c(60,150))+
+     scale_colour_manual(values = "black", name="")+
+     guides(fill = guide_legend(override.aes = list(shape = NA)),
+            colour = guide_legend(order=1))+
+     labs(caption = "Sources: FRED, Eurostat, prévisions OFCE. <br><i>Note de lecture : le solde public allemand était de 1.5% du PIB en 2019 et de -2.5% en <br> 2023. Il atteindrait -1.4% en 2025 selon les prévisions OFCE.</i>")
+   
+  )
+
+graph_dette_pbinter<- girafe(ggobj= graph_dette_pbinter,
+                             options = list(opts_sizing(rescale = TRUE, width = .5))
+)
+
+#graph_dette_pbinter
+
+deficits_data <- read.xlsx("data_fipu.xlsx", sheet = "datafipu") |> 
+  filter(mesure == "deficit") |> 
+  pivot_longer(-c(geo,mesure), names_to = "annee", values_to = "value") |> 
+  filter(annee %in% c(2019,2023,2025)) |> 
+  group_by(geo) |> 
+  pivot_wider(names_prefix = "d_", names_from = "annee", values_from = "value") |> 
+  mutate(consolid = d_2025-d_2023,
+         lollipop_start = d_2023,
+         d_2023 = case_when(d_2019 <0 ~  d_2023-d_2019,
+                            TRUE ~ d_2023),
+         across(where(is.numeric), ~ round(., digits = 1))
+  ) |> 
+  pivot_longer(c(d_2019,d_2023), names_to = "annee", values_to= "value" ) |> 
+  mutate(annee = factor(str_remove(annee,"d_"), levels = c("2019", "2023")),
+         tooltip_d_2019_2023 = case_when(annee == "2023" ~ str_c("Variation du solde 2019-2023 : <br>", lollipop_start-value[annee=="2019"]),
+                                         annee == "2019" ~ str_c("Solde 2019 :<br>", value[annee=="2019"])))
+
+
+graph_deficits_pbinter <-
+  (ggplot(deficits_data |> drop_na(),
+          aes(x=geo, y = value, fill = fct_rev(annee),  data_id=annee, tooltip=tooltip_d_2019_2023))+
+     geom_bar_interactive(stat="identity", position = "stack")+
+     theme_ofce()+
+     scale_fill_manual(values = c("firebrick3", "dodgerblue4"), name = "")+
+     geom_segment_interactive(aes(x=geo, xend=geo, y=lollipop_start, yend=d_2025, data_id="consolid", tooltip =str_c("Variation du solde 2023-2025 : <br>",consolid)),size=1.2)+
+     geom_point_interactive(aes(y=d_2025, data_id=d_2025, tooltip= str_c("Solde 2025 (prévision) : <br>", d_2025), color = "2025"), size = 3)+
+     xlab("")+
+     ylab("% du PIB")+
+     scale_colour_manual(values = "black", name="")+
+     guides(fill = guide_legend(override.aes = list(shape = NA)),
+            colour = guide_legend(order=1))+
+     labs(caption = "Sources: FRED, Eurostat, prévisions OFCE. <br><i>Note de lecture : le solde public allemand était de 1.5% du PIB en 2019 et de -2.5% en <br> 2023. Il atteindrait -1.4% en 2025 selon les prévisions OFCE.</i>")
+   
+  )
+
+graph_deficits_pbinter<- girafe(ggobj= graph_deficits_pbinter,
+                                options = list(opts_sizing(rescale = TRUE, width = .5))
+)
+
+#graph_deficits_pbinter
+
+save(list = ls(pattern="^graph_.+$"), 
+     file = "graphs_pbinter_fipu.rda")
+
+
+
+# Test : distinguish 2024 & 2025 (NOT USED) ------------------------------------------
+# 
+# deficits_data2 <- read.xlsx("data_fipu.xlsx", sheet = "datafipu") |> 
+#   filter(mesure == "deficit") |> 
+#   pivot_longer(-c(geo,mesure), names_to = "annee", values_to = "value") |> 
+#   filter(annee %in% c(2019,2023,2024,2025)) |> 
+#   group_by(geo) |> 
+#   pivot_wider(names_prefix = "d_", names_from = "annee", values_from = "value") |> 
+#   mutate(consolid2 = d_2025-d_2024,
+#          consolid1= d_2025-d_2024,
+#          lollipop_start = d_2023,
+#          d_2023 = case_when(d_2019 <0 ~  d_2023-d_2019,
+#                             TRUE ~ d_2023),
+#          across(where(is.numeric), ~ round(., digits = 1))
+#   ) |> 
+#   pivot_longer(c(d_2019,d_2023), names_to = "annee", values_to= "value" ) |> 
+#   mutate(annee = factor(str_remove(annee,"d_"), levels = c("2019", "2023")),
+#          tooltip_d_2019_2023 = case_when(annee == "2023" ~ str_c("Variation du solde 2019-2023 : <br>", lollipop_start-value[annee=="2019"]),
+#                                          annee == "2019" ~ str_c("Solde 2019 :<br>", value[annee=="2019"])))
+# 
+# 
+# 
+# graph_deficits_pbinter2 <-
+#   (ggplot(deficits_data2 |> drop_na(),
+#           aes(x=geo, y = value, fill = fct_rev(annee),  data_id=annee, tooltip=tooltip_d_2019_2023))+
+#      geom_bar_interactive(stat="identity", position = "stack")+
+#      theme_ofce()+
+#      scale_fill_manual(values = c("firebrick3", "dodgerblue4"), name = "")+
+#      geom_segment_interactive(aes(x=geo, xend=geo, y=lollipop_start, yend=d_2024,
+#                                   data_id="consolid1",
+#                                   tooltip =str_c("Variation du solde 2023-2024 : <br>",consolid2)),
+#                               size=1.2,
+#                               arrow =arrow(length = unit(0.03, "npc")))+
+#      geom_segment_interactive(aes(x=geo, xend=geo, y=d_2024, yend=d_2025,
+#                                     data_id="consolid1", 
+#                                     tooltip =str_c("Variation du solde 2024-2025 : <br>",consolid2)),
+#                                 size=1.2)+
+#      geom_point_interactive(aes(y=d_2025, data_id=d_2025, tooltip= str_c("Solde 2025 (prévision) : <br>", d_2025), color = "2025"), size = 3)+
+#      xlab("")+
+#      ylab("% du PIB")+
+#      scale_colour_manual(values = "black", name="")+
+#      guides(fill = guide_legend(override.aes = list(shape = NA)),
+#             colour = guide_legend(order=1))+
+#      labs(caption = "Sources: FRED, Eurostat, prévisions OFCE. <br><i>Note de lecture : le solde public allemand était de 1.5% du PIB en 2019 et de -2.5% en <br> 2023. Il atteindrait -1.4% en 2025 selon les prévisions OFCE.</i>")
+#    
+#   )
+# 
+# graph_deficits_pbinter2<- girafe(ggobj= graph_deficits_pbinter2,
+#                                 options = list(opts_sizing(rescale = TRUE, width = .5))
+# )
+# 
+# graph_deficits_pbinter2
